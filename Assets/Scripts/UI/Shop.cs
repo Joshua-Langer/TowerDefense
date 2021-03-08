f884d306d2bddbaedb943db5f750638214c9ff54
Joshua Langer <behistun@users.noreply.github.com>
2021-03-08 15:54:00 -0600

    Reorganized Prefabs, Refactored to 4 towers instead of 2.

--- Assets/Scripts/UI/Shop.cs
+++ Assets/Scripts/UI/Shop.cs
@@ -11,13 +11,19 @@ namespace TowerDefense.UI
     {
         public TowerController standardTower;
         public TowerController machineGunTower;
+        public TowerController cannonTower;
+        public TowerController rocketTower;
         public Text standardCost;
         public Text machineGunCost;
+        public Text cannonCost;
+        public Text rocketCost;
 
         void Awake()
         {
             standardCost.text = standardTower.levels[0].cost.ToString();
             machineGunCost.text = machineGunTower.levels[0].cost.ToString();
+            cannonCost.text = cannonTower.levels[0].cost.ToString();
+            rocketCost.text = rocketTower.levels[0].cost.ToString();
         }
 
 
@@ -29,6 +35,17 @@ namespace TowerDefense.UI
         public void SelectMachineGunTower()
         {
             BuildManager.Instance.SelectTowerToBuild(machineGunTower);
+        }
+
+        public void SelectCannonTower()
+        {
+            BuildManager.Instance.SelectTowerToBuild(cannonTower);
+        }
+
+        public void SelectRocketTower()
+        {
+            BuildManager.Instance.SelectTowerToBuild(rocketTower);
+
         }
     }
 }
